bd328701529a0d3555c9109565aba70efc6607cc
EvyOliveira <evelynoliveira.eo@gmail.com>
2024-07-02 19:50:41 -0300

    Add service layer

--- 9-HexagonalArchitecture/application/product.go
+++ 9-HexagonalArchitecture/application/product.go
@@ -15,7 +15,7 @@ type ProductInterface interface {
 	IsValid() (bool, error)
 	Enable() error
 	Disable() error
-	GetId() string
+	GetID() string
 	GetName() string
 	GetStatus() string
 	GetPrice() float64
