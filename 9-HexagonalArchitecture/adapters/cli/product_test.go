b5a704369dcae38bc2ca54573a0b605883ebaeb0
EvyOliveira <evelynoliveira.eo@gmail.com>
2024-08-06 11:00:53 -0300

    Test adapter layer command line interface

---
+++ 9-HexagonalArchitecture/adapters/cli/product_test.go
@@ -0,0 +1,53 @@
+package cli_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/EvyOliveira/fullCycle/adapters/cli"
+	mock_application "github.com/EvyOliveira/fullCycle/application/mocks"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRun(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	productName := "Silver Tape"
+	productPrice := 25.55
+	productStatus := "enabled"
+	productId := "abcd"
+
+	productMock := mock_application.NewMockProductInterface(controller)
+	productMock.EXPECT().GetID().Return(productId).AnyTimes()
+	productMock.EXPECT().GetStatus().Return(productStatus).AnyTimes()
+	productMock.EXPECT().GetPrice().Return(productPrice).AnyTimes()
+	productMock.EXPECT().GetName().Return(productName).AnyTimes()
+
+	service := mock_application.NewMockProductServiceInterface(controller)
+	service.EXPECT().Create(productName, productPrice).Return(productMock, nil).AnyTimes()
+	service.EXPECT().Get(productId).Return(productMock, nil).AnyTimes()
+	service.EXPECT().Enable(gomock.Any()).Return(productMock, nil).AnyTimes()
+	service.EXPECT().Disable(gomock.Any()).Return(productMock, nil).AnyTimes()
+
+	resultExpected := fmt.Sprintf("Product ID %s with the name %s has been created with the price %f and status %s", productId, productName, productPrice, productStatus)
+	result, err := cli.Run(service, "create", "", productName, productPrice)
+	require.Nil(t, err)
+	require.Equal(t, resultExpected, result)
+
+	resultExpected = fmt.Sprintf("Product %s has been enabled.", productName)
+	result, err = cli.Run(service, "enabled", productId, "", 0)
+	require.Nil(t, err)
+	require.Equal(t, resultExpected, result)
+
+	resultExpected = fmt.Sprintf("Product %s has been disabled.", productName)
+	result, err = cli.Run(service, "disabled", productId, "", 0)
+	require.Nil(t, err)
+	require.Equal(t, resultExpected, result)
+
+	resultExpected = fmt.Sprintf("ProductID: %s\nName: %s\nPrice: %f\nStatus: %s", productId, productName, productPrice, productStatus)
+	result, err = cli.Run(service, "get", productId, "", 0)
+	require.Nil(t, err)
+	require.Equal(t, resultExpected, result)
+}
