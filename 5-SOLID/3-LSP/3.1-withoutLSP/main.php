991692d4d7e46cb09db301315a757a4def915593
EvyOliveira <evelynoliveira.eo@gmail.com>
2024-08-08 12:16:02 -0300

    Add liskov substitution principle

---
+++ 5-SOLID/3-LSP/3.1-withoutLSP/main.php
@@ -0,0 +1,27 @@
+<?php
+
+class Movie
+{
+    public function play()
+    {
+        // trecho de código aqui
+    }
+
+    public function increaseVolume()
+    {
+        // trecho de código aqui
+    }
+}
+
+class TheLionKing extends Movie 
+{
+    // trecho de código aqui
+}
+
+class ModernTimes extends Movie
+{
+    public function increaseVolume()
+    {
+        // trecho de código aqui
+    }
+}
---
+++ 5-SOLID/3-LSP/3.2-usingLSP/main.php
@@ -0,0 +1,24 @@
+<?php
+
+class Movie 
+{
+    public function play() 
+    {
+        // trecho de código aqui
+    }
+
+    public function increaseVolume()
+    {
+      // trecho de código aqui  
+    }
+}
+
+class TheLionKing extends Movie
+{
+    // trecho de código aqui 
+}
+
+class ModernTimes 
+{
+    // trecho de código aqui 
+}
